32bda1677dffef75f2d364ce765623358a23dc12
Andres Vervaecke <andres.vervaecke@ml6.eu>
2022-12-01 10:03:47 +0100

    add logs

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 export JOB_SERVER_PORT=8080  # Same port as before
-export OUTPUT_FILE="output"
 export PATH=/Users/andresvervaecke/tools/apache-maven-3.8.6/bin:$PATH
+
+export OUTPUT_FILE="src/main/resources/output"
+export INPUT_FILE="src/main/resources/input.csv"
+export MODEL_PATH="src/main/resources/model"
 
 cd multi-language-beam
 mvn compile exec:java -Dexec.mainClass=org.apache.beam.examples.multilanguage.SklearnMnistClassification \
     -Dexec.args="--runner=PortableRunner \
                  --jobEndpoint=localhost:$JOB_SERVER_PORT \
-                 --output=$OUTPUT_FILE" \
+                 --output=$OUTPUT_FILE \
+                 --input=$INPUT_FILE \
+                 --modelPath=$MODEL_PATH" \
     -Pportable-runner
